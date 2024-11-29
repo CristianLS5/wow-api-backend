9e54d35a36c02a472f03e81d9e225606b9064a9a
CristianLS5 <cristianlopezc4@gmail.com>
2024-11-29 17:03:53 +0100

    add databse session parameters

--- server/src/config/sessionStore.ts
+++ server/src/config/sessionStore.ts
@@ -9,6 +9,16 @@ export const initializeStore = (mongoUri: string): Store => {
     ttl: 30 * 24 * 60 * 60, // 30 days in seconds
     collectionName: "sessions",
     dbName: "wow_character_viewer",
+    mongoOptions: {
+      ssl: true,
+      tls: true,
+      tlsInsecure: true,
+      tlsAllowInvalidCertificates: true,
+      retryWrites: true,
+      minPoolSize: 0,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+    }
   });
   return store;
 };
