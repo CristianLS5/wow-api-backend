eb33c1b01dfa4445e1c5ff4eb41f613ac7345cdd
CristianLS5 <cristianlopezc4@gmail.com>
2024-11-30 13:38:10 +0100

    update auth

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get("/bnet", authController.getAuthorizationUrl);
 router.get("/callback", authController.handleCallback);
+router.post("/callback", authController.handleOAuthCallback);
 router.get("/validate", authController.validateToken);
 router.post("/logout", authController.logout);
 router.post('/exchange-token', authController.exchangeToken);
